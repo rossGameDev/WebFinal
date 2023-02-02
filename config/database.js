abc8749e7cfefd8ac5640db48d638fac103c3c8b
Ethan <emross@email.neit.edu>
2023-02-02 14:15:44 -0500

    fixed

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 if(process.env.NODE_ENV === "production")
 {
-    module.exports = {MongoURI:"mongodb+srv://Unmuted:Blackadder01@cluster0.up9tvrz.mongodb.net/?retryWrites=true&w=majority"};
+    module.exports = {mongoURI:"mongodb+srv://Unmuted:Blackadder01@cluster0.up9tvrz.mongodb.net/?retryWrites=true&w=majority"};
 }
 else
 {
-    module.exports = {MongoURI:"mongodb://localhost:27017/gameEntries"};
+    module.exports = {mongoURI:"mongodb://localhost:27017/gameEntries"};
 }
